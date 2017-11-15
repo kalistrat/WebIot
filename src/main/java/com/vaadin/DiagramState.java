88ac53ce37a605473eb498a8befe296392f2126b
kalistrat <n7.semenov@gmail.com>
2017-11-15 18:18:10 +0300

    Добавил русский язык в график
    Добавил русский язык в график

--- src/main/java/com/vaadin/DiagramState.java
+++ src/main/java/com/vaadin/DiagramState.java
@@ -8,13 +8,13 @@ import java.util.List;
  * Created by kalistrat on 13.11.2017.
  */
 public class DiagramState extends JavaScriptComponentState {
-    private String graphFileName;
+    private List<tDetectorDiagramData> measuresData;
 
-    public String getGraphFileName() {
-        return graphFileName;
+    public List<tDetectorDiagramData> getGraphData() {
+        return measuresData;
     }
 
-    public void setGraphFileName(String fileName) {
-        this.graphFileName = fileName;
+    public void setGraphData(List<tDetectorDiagramData> listData) {
+        this.measuresData = listData;
     }
 }
---
+++ src/main/java/com/vaadin/tDetectorDiagramData.java
@@ -0,0 +1,13 @@
+package com.vaadin;
+
+/**
+ * Created by kalistrat on 15.11.2017.
+ */
+public class tDetectorDiagramData {
+    String mDate;
+    Double mVal;
+    public tDetectorDiagramData(String mdate,double mval){
+        mDate = mdate;
+        mVal = mval;
+    }
+}
